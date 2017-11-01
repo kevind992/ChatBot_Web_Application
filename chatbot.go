d849d32fdca945d42c9846a44859e8639e9aa162
KEVIN DELASSUS - STUDENT <G00270791@gmit.ie>
2017-11-01 10:21:53 +0000

    User input working

--- chatbot.go
+++ chatbot.go
@@ -3,14 +3,22 @@
 package main
 
 import (
-	//"fmt"
-	"net/http"
-)
 
+	"fmt"
+	"net/http"
+
+)
+func userinputhandler(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, "Hello, %s!, How are you today?", r.URL.Query().Get("value")) //.Path[1:])
+
+}
 func main() {
 
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/", fs)
-	
+
+	http.HandleFunc("/user-input", userinputhandler)
+
 	http.ListenAndServe(":8080", nil)
 }
