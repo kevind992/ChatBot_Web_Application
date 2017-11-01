81a6d36b6d5ff424e7292bf86ff885841e9d3053
KEVIN DELASSUS - STUDENT <G00270791@gmit.ie>
2017-11-01 10:36:35 +0000

    muti response in output box

--- chatbot.go
+++ chatbot.go
@@ -10,9 +10,10 @@ import (
 )
 func userinputhandler(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Fprintf(w, "Hello, %s!, How are you today?", r.URL.Query().Get("value")) //.Path[1:])
+	fmt.Fprintf(w,"User : %s \n", r.URL.Query().Get("value"))
+	fmt.Fprintf(w, "Chatbot : Hello, %s!, How are you today?", r.URL.Query().Get("value")) //.Path[1:])
 
-	
+
 
 }
 func main() {
