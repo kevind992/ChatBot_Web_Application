713687eee0da9a5d603c19e54bfef8768e8b4f8d
Kevin Delassus <g00270791@gmit.ie>
2017-11-20 15:23:49 +0000

    File System Update

--- eliza/eliza.go
+++ eliza/eliza.go
@@ -1,185 +1,258 @@
 package eliza
 
-import (
-	"fmt"
-	"bufio"
-	//"fmt"
-	"log"
-	"math/rand"
-	"os"
+import(
+
 	"regexp"
-	"strconv"
+	"time"
+	"math/rand"
+	//"fmt"
 	"strings"
+
 )
 
-// A data structure representing a term that should be replaced in a string.
-// original is a regular expression to be matched, and substitute is a string to replace the match with.
-// An example use is to replace the word you with the word me.
-type substitution struct {
-	original   *regexp.Regexp
-	substitute string
-}
 
-// A data structure representing a user input and a list of responses to it that Eliza can give.
-// question is a regular expression representing the user input.
-// answers is an array of strings, any of which is a reasonable response to question.
-// question can capture groups of characters, and elements of answers can use them.
-// $1 is the first match, $2 the second, etc.
-type response struct {
-	question *regexp.Regexp
-	answers  []string
-}
+func ElizaStart(userInput string) string{
 
-// Eliza is a data structure representing a psychoanalyst.
-// responses is an array containing elements of type response, as above.
-// Likewise, substitutions is an array containing elements of type substitution.
-// The order of the elements in both arrays matters - the responses and substitutions are matched in order.
-type Eliza struct {
-	responses     []response
-	substitutions []substitution
-}
+	rand.Seed(time.Now().UTC().UnixNano())
 
-// Method to read in a text file containing substitutions data.
-// It takes a single argument, a string, which is the path to the substitutions data file.
-// The file should have the following format:
-//   All lines that begin with a hash symbol are comments, and are ignored.
-//   Each section of the file should begin with at least one blank line.
-//   The next line should be a regular expression for what to substitute.
-//   The next line should be the new text for the substitution.
-//   After that, there should be at least one blank.
-// An example substitutions file is given in data/substitutions.txt.
-func (me *Eliza) readsubstitutions(path string) {
+	//userInput = Reflect(userInput)
 
-	// Open the file, logging a fatal error if it fails.
-	file, err := os.Open(path)
-	if err != nil {
-		log.Fatal(err)
+	ranNum := rand.Intn(3)
+
+	if matched, _ := regexp.MatchString(`(?i).*\bfather\b.*`, userInput); matched {
+		return "Why don't you tell me more about your Father?"
 	}
 
-	// Set up a line-by-line scanner for the file.
-	scanner := bufio.NewScanner(bufio.NewReader(file))
-	scanner.Split(bufio.ScanLines)
+	re1 := regexp.MustCompile("Hi, my name is ([^.!?]*)[.!?]?")
+	if re1.MatchString(userInput){
+		return re1.ReplaceAllString(userInput,"Hi $1, how are you today?")
+	}
+	re1 = regexp.MustCompile(`(?im)^\s*I need ([^\.!]*)[\.!]*\s*$`)
+	if re1.MatchString(userInput){
+		a1 := []string{
+			`Why do you need $1?`,
+			`Would it really help you to get $1?`,
+			`Are you sure you need $1?`,}
 
-	// Read through the file line by line.
-	// readoriginal is false if we have not yet read the regular expression to match.
-	// It is true if we have read the regular expression, and are now looking for the substitution string.
-	for readoriginal := false; scanner.Scan(); {
-		// Get the text on the current line.
-		s := scanner.Text()
+		return re1.ReplaceAllString(userInput, a1[ranNum])
+	}
 
-		// Decide what to do with the line.
-		switch {
-		// If the line is blank or starts with a # character then skip it.
-		case strings.HasPrefix(s, "#") || len(s) == 0:
-			// Do nothing
-
-		// If we haven't read the original, then append an element to the substitutions array.
-		// The regualr expression is compiled, and the substitution is left blank for now.
-		case readoriginal == false:
-			me.substitutions = append(me.substitutions, substitution{original: regexp.MustCompile(s)})
-			readoriginal = true
-		// Otherwise read the substitution and assign it to the last element of the substitutions array.
-		default:
-			me.substitutions[len(me.substitutions)-1].substitute = s
-			readoriginal = false
+	re1 = regexp.MustCompile(`(?im)^\s*Why don'?t you ([^\?]*)\?*\s*$`)
+	if re1.MatchString(userInput){
+		a2 := []string{
+			"Do you really think I don't $1?",
+			"Perhaps eventually I will $1?",
+			"Do you really want me to $1?",}
+			return re1.ReplaceAllString(userInput, a2[ranNum])
+		
+	}
+	re1 = regexp.MustCompile(`(?im)^\s*Why can'?t I ([^\?]*)\?*\s*$`)
+	if re1.MatchString(userInput){
+		a3 := []string{
+			"Do you think you should be able to $1?",
+			"If you could $1, what would you do?",
+			"I don't know - why can't you $1?",
+			"Have you really tried?",}
+			return re1.ReplaceAllString(userInput, a3[ranNum])
+		
+	}
+	re1 = regexp.MustCompile(`(?im)^\s*I can'?t ([^\.!]*)[\.!]*\s*$`)
+	if re1.MatchString(userInput){
+		a4 := []string{
+			"How do you know you can't $1",
+			"Perhaps you could $1 if you tried.",
+			"What would it take for you to $1",}
+			return re1.ReplaceAllString(userInput, a4[ranNum])
+		
+	}
+	re1 = regexp.MustCompile(`(?im)^\s*I am ([^\.!]*)[\.!]*\s*$`)
+	if re1.MatchString(userInput){
+		a5 := []string{
+			"Did you come to me because you are $1",
+			"How long have you been $1",
+			"How do you feel about being $1",}
+			return re1.ReplaceAllString(userInput, a5[ranNum])
+		
+	}
+	re1 = regexp.MustCompile(`(?im)^\s*I'm ([^\.!]*)[\.!]*\s*$`)
+	if re1.MatchString(userInput){
+		a6 := []string{
+			"How does being $1 make you feel",
+			"Do you enjoy being $1",
+			"Why do you tell me you're $1",
+			"Why do you think you're $1",}
+			return re1.ReplaceAllString(userInput, a6[ranNum])
+		
+	}
+	re1 = regexp.MustCompile(`(?im)^\s*How [^\?]*\?*\s*$`)
+	if re1.MatchString(userInput){
+		a7 := []string{
+			"How do you suppose?",
+			"Perhaps you can answer your own question.",
+			"What is it you're really asking",}
+			return re1.ReplaceAllString(userInput, a7[ranNum])
+		
+	}
+	re1 = regexp.MustCompile(`(?im)^\s*Yes\s*$`)
+	if re1.MatchString(userInput){
+		a8 := []string{
+			"You seem quite sure.",
+			"Interesting...",
+			"OK, but can you elaborate a bit?",}
+			return re1.ReplaceAllString(userInput, a8[ranNum])
+		
+	}
+	re1 = regexp.MustCompile(`(?im)^\s*I have ([^\.!]*)[\.!]*\s*$`)
+	if re1.MatchString(userInput){
+		a9 := []string{
+			"Why do you tell me you've $1",
+			"Have you really $1",
+			"Now that you have $1, what will you do next?",}
+			return re1.ReplaceAllString(userInput, a9[ranNum])
+		
+	}
+	re1 = regexp.MustCompile(`(?im)^\s*Is there ([^\?]*)\?*\s*$`)
+	if re1.MatchString(userInput){
+		a10 := []string{
+			"Do you think there is $1",
+			"It's likely that there is $1",
+			"Would you like there to be",}
+			return re1.ReplaceAllString(userInput, a10[ranNum])
+		
+	}
+	re1 = regexp.MustCompile(`(?im)^\s*You ([^\.!]*)[\.!]*\s*$`)
+	if re1.MatchString(userInput){
+		a11 := []string{
+			"We should be discussing you, not me.",
+			"Why do you say that about me?",
+			"Why do you care whether I $1?",}
+			return re1.ReplaceAllString(userInput, a11[ranNum])
+		
+	}
+	re1 = regexp.MustCompile(`(?im)^\s*Why ([^\?]*)\?*\s*$`)
+	if re1.MatchString(userInput){
+		a12 := []string{
+			"Why don't you tell me the reason why $1?",
+			"Why do you think $1?",}
+			return re1.ReplaceAllString(userInput, a12[ranNum])
+		
+	}
+	re1 = regexp.MustCompile(`(?im)^\s*I want ([^\.!]*)[\.!]*\s*$`)
+	if re1.MatchString(userInput){
+		a13 := []string{
+			"What would it mean to you if you got $1",
+			"Why do you want $1",
+			"What would you do if you got $1?",
+			"If you got $1, then what would you do?",}
+			return re1.ReplaceAllString(userInput, a13[ranNum])
+		
+	}
+	re1 = regexp.MustCompile(`[^\?]*\?*\s*$`)
+	if re1.MatchString(userInput){
+		a14 := []string{
+			"Why do you ask that?",
+			"Please consider wheather you can answer your own question.",
+			"Perhaps the answer lies within yourself?",
+			"Why don't you tell me?",}
+			return re1.ReplaceAllString(userInput, a14[ranNum])
+		
+	}
+	re1 = regexp.MustCompile(`(?im)^\s*quit\s*$`)
+	if re1.MatchString(userInput){
+		a15 := []string{
+			"Thank you for visiting.",
+			"Bye now, thanks for visiting.",
+			"Have a nice day.",}
+			return re1.ReplaceAllString(userInput, a15[ranNum])
+		
+	}
+	re1 = regexp.MustCompile(`(?im)^\s*You are ([^\.!]*)[\.!]*\s*$`)
+	if re1.MatchString(userInput){
+		a16 := []string{
+			"Why do you think I am $1?",
+			"Does it please you to think that I'm $1",
+			"Perhaps you would like me to be $1",
+			"Perhaps you're really talking about yourself?",}
+			return re1.ReplaceAllString(userInput, a16[ranNum])
+		
+	}
+	re1 = regexp.MustCompile(`(?im)^\s*You're ([^\.!]*)[\.!]*\s*$`)
+	if re1.MatchString(userInput){
+		a17 := []string{
+			"Why do you say I am $1?",
+			"Why do you think I am $1?",
+			"Are we talking about you, or me?",}
+			return re1.ReplaceAllString(userInput, a17[ranNum])
+		
+	}
+	re1 = regexp.MustCompile(`(?im)^\s*I don't ([^\.!]*)[\.!]*\s*$`)
+	if re1.MatchString(userInput){
+		a18 := []string{
+			"Don't you really $1?",
+			"Why don't you $1",
+			"Do you want to $1?",}
+			return re1.ReplaceAllString(userInput, a18[ranNum])
+		
+	}
+	re1 = regexp.MustCompile(`(?im)^\s*quit\s*$`)
+	if re1.MatchString(userInput){
+		a19 := []string{
+			"Thank you for visiting.",
+			"Bye now, thanks for visiting.",
+			"Have a nice day.",}
+			return re1.ReplaceAllString(userInput, a19[ranNum])
+		
+	}
+	re1 = regexp.MustCompile(`(.*)`)
+	if re1.MatchString(userInput){
+		a20 := []string{
+			"Please tell me more.",
+			"Let's change focus a bit... Tell me about your family",
+			"Can you elaborate on that?",
+			"I see.",
+			"Why do you that $1?",
+			"Very interesting.",
+			"$1",
+			"I see, and what does that tell you?",
 		}
+			return re1.ReplaceAllString(userInput, a20[ranNum])
+		
 	}
+	return ""
 }
+func Reflect(input string)string{
 
-// Function to read in a text file containing responses data.
-// The file should have the following format:
-// All lines that begin with a hash symbol are comments, and are ignored.
-// This file should have the following format:
-//   Each section of the file should begin with at least one blank line.
-//   The next line should be a regular expression, matching a user input.
-//   Each subsequent line, until a blank line, should contain a response to
-//   the usr input. One of these will be chosen at random upon user input.
-//   After the responses, there should be at least one blank.
-// An example responses file is given in data/responses.txt.
-func (me *Eliza) readresponses(path string) {
-	// Open the file, and quit on an error.
-	file, err := os.Open(path)
-	if err != nil {
-		log.Fatal(err)
+	// Split the input on word boundaries.
+	boundaries := regexp.MustCompile(`\b`)
+	tokens := boundaries.Split(input, -1)
+
+	reflections := [][]string{
+		{`am`,`are`},
+		{`was`,`were`},
+		{`i`,`you`},
+		{`i'd`,`you would`},
+		{`i've`,`you have`},
+		{`i'll`,`you will`},
+		{`my`,`your`},
+		{`are`,`am`},
+		{`you've`,`I have`},
+		{`you'll`,`I will`},
+		{`your`,`my`},
+		{`yours`,`mine`},
+		{`you`,`me`},
+		{`me`,`you`},
 	}
 
-	// Set up a buffer to read the file, line by line.
-	scanner := bufio.NewScanner(bufio.NewReader(file))
-	scanner.Split(bufio.ScanLines)
-
-	// Loop through the lines of the file, initialising a flag called newsection to true.
-	for newsection := true; scanner.Scan(); {
-		// Get the next line of the file, assign it to s.
-		s := scanner.Text()
-
-		// Decide what to do, based on the following rules.
-		// Note that without a condition, switch in Go if just like if-else.
-		// Also, the clauses break automatically.
-		switch {
-		// Do nothing if the line is a comment (begins with #).
-		case strings.HasPrefix(s, "#"):
-		// If the line is blank, presume we are starting a new section.
-		case len(s) == 0:
-			newsection = true
-		// If newsection is true then create a new response item with the line as a question.
-		// Then set newsection to false.
-		case newsection == true:
-			me.responses = append(me.responses, response{question: regexp.MustCompile(s)})
-			newsection = false
-		// Otherwise we're just reading a possible response, adding it to the last response item.
-		default:
-			me.responses[len(me.responses)-1].answers = append(me.responses[len(me.responses)-1].answers, s)
-		}
-	}
-}
-
-// This function accepts a user input, and gives a response as Eliza.
-func (me *Eliza) analyse(userinput string) string {
-	// Loop through the responses, looking for a match for the user input.
-	for _, response := range me.responses {
-		if matches := response.question.FindStringSubmatch(userinput); matches != nil {
-
-			// Select a random answer.
-			answer := response.answers[rand.Intn(len(response.answers))]
-
-			// Fill the answer with the captured groups from the matches.
-			for i, match := range matches[1:] {
-				// Reflect the pronouns in the captured group.
-				for _, sub := range me.substitutions {
-					match = sub.original.ReplaceAllString(match, sub.substitute)
-					// Remove any spaces at the start or end.
-					match = strings.TrimSpace(match)
-				}
-				// Replace $1 with the first reflected captured group, $2 with the second, etc.
-				answer = strings.Replace(answer, "$"+strconv.Itoa(i+1), match, -1)
+	// Loop through each token, reflecting it if there's a match.
+	for i, token := range tokens {
+		for _, reflection := range reflections {
+			if matched, _ := regexp.MatchString(reflection[0], token); matched {
+				tokens[i] = reflection[1]
+				break
 			}
-
-			// Clear any ~~ markers from the string. They prevent future matches.
-			answer = strings.Replace(answer, "~~", "", -1)
-
-			// Send the filled answer back.
-			return answer
 		}
 	}
-
-	return "I don't know what to say."
-}
-
-// Program entry point.
-func ElizaStart(userinput string) string{
-	// Create a new instance of Eliza.
-	eliza := Eliza{}
-
-	fmt.Println("You are in the eliza class: ",userinput)
-
-	// Read the substitutions file.
-	eliza.readsubstitutions("data/substitutions.txt")
-	// Read the responses file.
-	eliza.readresponses("data/responses.txt")
-
-	elizaResponse := eliza.analyse(userinput)
-
 	
-	
-	return elizaResponse
+	// Put the tokens back together.
+	return strings.Join(tokens, ``)
 }
